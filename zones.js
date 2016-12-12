c002842ef5079a9c2966ed6fe9c1923441fb1626
Elif Koc <elifkoc@nyu.edu>
2016-12-12 18:03:58 -0500

    Better hoop

--- zones.js
+++ zones.js
@@ -11,10 +11,24 @@ function Zones() {
   						side:'double',
   						clickFunction: function(moveToZone) {
   						  //How do you move to this spot, but look up? 
-          			world.teleportToObject(moveToZone);
+          			world.camera.setPosition(-18, 5, 0);
 			        }
   					});
   	world.add(zone1);
+  	
+  	var zone1height = new Plane({
+  						x: -18, y:-4.9, z:0,
+  						width: 5, height:5,
+  						red:255, green:255, blue:255,
+  						opacity: 0,
+  						rotationX: -90,
+  						side:'double',
+  						clickFunction: function(moveToZone) {
+  						  //How do you move to this spot, but look up? 
+          			world.teleportToObject(moveToZone);
+			        }
+  					});
+  	world.add(zone1height);
   	
   }
 }
